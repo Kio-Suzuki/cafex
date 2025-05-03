7b725eb806d5a14148c22a673c402edd55799f2d
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2025-05-03 15:40:07 -0300

    feat: adicionando botao e teste e2e com cypress

---
+++ cafex/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <Botao :funcao="handleClick" :label="label" />
+  </main>
+</template>
+
+<script setup>
+import Botao from './components/Botao.vue'
+
+const handleClick = () => {
+  console.log('Clicou no botão')
+}
+
+const label = 'Clique aqui'
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ cafex/src/components/Botao.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="funcao">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Botao',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    funcao: {
+      type: Function,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
