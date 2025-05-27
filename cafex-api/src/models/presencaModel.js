fc28bc3729560e2de61c74dcc267fb0f48321113
Kauan Borotto <kauanborotto@gmail.com>
2025-05-27 19:55:01 -0300

    # Adicionado fluxo de Presenca

---
+++ cafex-api/src/models/presencaModel.js
@@ -0,0 +1,34 @@
+import prisma from '../database/prisma.js';
+
+class PresencaModel {
+    static async create(data) {
+        return prisma.presenca.create({
+            data,
+        });
+    }
+
+    static async getAll(where) {
+        return prisma.presenca.findMany({ where });
+    }
+
+    static async getById(id) {
+        return prisma.presenca.findUnique({
+            where: { id },
+        });
+    }
+
+    static async update(id, data) {
+        return prisma.presenca.update({
+            where: { id },
+            data,
+        });
+    }
+
+    static async delete(id) {
+        return prisma.presenca.delete({
+            where: { id },
+        });
+    }
+}
+
+export default PresencaModel;
